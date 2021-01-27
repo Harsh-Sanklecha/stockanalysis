a4825027440820a1f1127a22f5ee0e7cc3317c3d
Harsh-Sanklecha <sanklecha.harsh@gmail.com>
2021-01-27 13:25:49 +0530

    Email verification

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from .views import emailVerification
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -7,6 +8,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/',views.register, name='register'),
+    path('activate/<uidb64>/<token>',emailVerification.as_view(), name='activate'),
     path('accounts/login/',views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout_view'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from django.urls import reverse
+from django.views import *
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from main.decorators import unauthenticated_user
+from django.contrib.auth.models import User
 from newsapi.newsapi_client import NewsApiClient
 from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
+
+from main.decorators import unauthenticated_user
+from .forms import UserRegisterForm
+from .utils import account_activation_token
 
 @unauthenticated_user
 def index(request):
@@ -45,12 +53,23 @@ def register(request):
             user.is_active = False
             user.save()
 
+            current_site = get_current_site(request)
+
             email_subject = "Confirm your account on InvestInIt"
-            email_body = "TEST"
+            email_body = {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            }
+            link = reverse('activate', kwargs={
+                'uidb64': email_body['uid'], 'token': email_body['token']})
+            activate_url = 'http://'+current_site.domain+link
 
             email = EmailMessage(
                 email_subject,
-                email_body,
+                'Hi '+user.username + ',\n\nThanks for signing up with Heroku! You must follow this link to activate your account: \n'+activate_url 
+                + '\n\nHave fun, and don''t hesitate to contact us with your feedback.\nInvest In It Team \nhttp://www.investinit.ml',
                'datafeed0@gmail.com',
                [email_id],
             )
@@ -58,11 +77,33 @@ def register(request):
             email.send(fail_silently=False)
             
 
-            messages.success(request, 'Account created successfully')
+            messages.success(request, 'Account Created Successfully. Please verify your E-Mail')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form':form })
+
+class emailVerification(View):
+    def get(self, request, uidb64, token):
+        try:
+            id = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=id)
+
+            if not account_activation_token.check_token(user, token):
+                return redirect('login'+'?message='+'User already activated')
+
+            if user.is_active:
+                return redirect('login')
+            user.is_active = True
+            user.save()
+
+            messages.success(request, 'Account Activated Successfully')
+            return redirect('login')
+
+        except Exception as ex:
+            pass
+
+        return redirect('login')
 
 
 @unauthenticated_user
