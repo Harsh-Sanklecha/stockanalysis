91eff4f43b4d3a11193e44bb1dcf30cdde53665f
Harsh-Sanklecha <sanklecha.harsh@gmail.com>
2021-01-29 12:28:32 +0530

    Forgot Password

--- stockanalysis/settings.py
+++ stockanalysis/settings.py
@@ -107,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'datafeed0@gmail.com'
