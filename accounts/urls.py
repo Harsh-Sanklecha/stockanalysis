92e3bcf73330af3caecb5d997c86d5ecbb2f53e1
Harsh-Sanklecha <sanklecha.harsh@gmail.com>
2021-01-30 13:57:58 +0530

    Password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('activate/<uidb64>/<token>',emailVerification.as_view(), name='activate'),
     path('accounts/login/',views.login_view, name='login'),
     path('reset_password/',auth_views.PasswordResetView.as_view(),name='reset_password'),
-    path('reset_password_done/',auth_views.PasswordResetView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>',auth_views.PasswordResetView.as_view(),name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetView.as_view(),name='password_reset_complete'),
+    path('reset_password_done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     path('logout/', views.logout_view, name='logout_view'),
 ]
